e1fb2426a85d3b319cb3826cef96d8cb56082a84
lex <me@ellcs.de>
2022-06-16 17:59:33 +0200

    Initialize project

---
+++ README.md
@@ -0,0 +1,4 @@
+# iceity
+
+This project aims to be a dropin replacement for zenity. Zenity allows you
+to create GUI prompts from the terminal. 
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+
+//   -h, --help                                         
+//   --help-all                                         
+//   --help-general                                     
+//   --help-calendar                                    
+//   --help-entry                                       
+//   --help-error                                       
+//   --help-info                                        
+//   --help-file-selection                              
+//   --help-list                                        
+//   --help-notification                                
+//   --help-progress                                    
+//   --help-question                                    
+//   --help-warning                                     
+//   --help-scale                                       
+//   --help-text-info                                   
+//   --help-color-selection                             
+//   --help-password                                    
+//   --help-forms                                       
+//   --help-misc                                        
+//   --help-gtk                                         
+// 
+//   --calendar                                         
+//   --entry                                            
+//   --error                                            
+//   --info                                             
+//   --file-selection                                   
+//   --list                                             
+//   --notification                                     
+//   --progress                                         
+//   --question                                         
+//   --warning                                          
+//   --scale                                            
+//   --text-info                                        
+//   --color-selection                                  
+//   --password                                         
+//   --forms                                            
+//   --display=DISPLAY                                  
+
+fn main() {
+    println!("Hello, world!");
+}
