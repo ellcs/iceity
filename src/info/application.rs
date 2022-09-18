469716d870b59e150247824d31508883846e372c
ellcs <me@ellcs.de>
2022-09-18 15:11:53 +0200

    Remove all warnings

--- src/info/application.rs
+++ src/info/application.rs
@@ -12,6 +12,7 @@ pub struct InfoDialog {
     pub ok: button::State,
 }
 
+#[allow(dead_code)]
 impl Application for InfoDialog {
     type Executor = executor::Default;
     type Message = InfoMessage;
@@ -36,7 +37,7 @@ impl Application for InfoDialog {
                 std::process::exit(0);
             }
         }
-        Command::none()
+        // Command::none()
     }
 
     fn view(&mut self) -> Element<InfoMessage> {
